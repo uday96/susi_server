2ba3fbe91d6f4813723986c7b5aa665427adbb92
Uday <udayteja96@gmail.com>
2017-07-20 21:12:01 +0530

    replace cmd for kubernetes deploy

--- kubernetes/deploy.sh
+++ kubernetes/deploy.sh
@@ -7,4 +7,10 @@ if [ "$1" = "create" ]; then
     echo "Waiting for server to start up. ~30s."
     sleep 30
     echo "Done. The project was deployed to kubernetes. :)"
+elif [ "$1" = "replace" ]; then
+    echo "Re-Deploying the project to kubernetes cluster"
+    kubectl replace -R -f ${DIR}/yamls/susi-server
+    echo "Waiting for server to start up. ~30s."
+    sleep 30
+    echo "Done. The project was re-deployed to kubernetes. :)"
 fi
